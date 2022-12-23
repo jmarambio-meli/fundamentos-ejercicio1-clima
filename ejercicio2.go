3f02da30284629da5eccee1f4a21b4ae1cc4bc80
jmarambio-meli <121036444+jmarambio-meli@users.noreply.github.com>
2022-12-23 13:36:26 -0300

    ejercicio2

---
+++ ejercicio2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var temperatura float64 = 20.5
+	var humedad float64 = 16.3
+	var presion float64 = 15.3
+
+	fmt.Println(temperatura, humedad, presion)
+}
